d09bb10de5ef32649d39a72152522095a2752968
Satyam Singh <officialsatyam1010@gmail.com>
2023-04-04 22:30:41 +0530

    added new route

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3003;
 // for serving static files
 // app.use(express.static('public'));
 
-app.get("/health", (req, res) => {
+app.get("/admin/test", (req, res) => {
     return res.status(200).json({message: "I am Healthy Don't Worry!", status: 200})
 })
 
